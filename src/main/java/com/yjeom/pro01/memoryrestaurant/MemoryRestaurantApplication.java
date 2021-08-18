412605a083030a0bf6a6f9f2ca2e627bb096895c
yjeom <eomyj0920@gmail.com>
2021-08-18 16:18:59 +0900

    Initial commit

---
+++ src/main/java/com/yjeom/pro01/memoryrestaurant/MemoryRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.yjeom.pro01.memoryrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoryRestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yjeom/pro01/memoryrestaurant/MemoryRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yjeom.pro01.memoryrestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoryRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
