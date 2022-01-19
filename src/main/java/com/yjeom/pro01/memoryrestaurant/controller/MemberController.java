9884173c76bc03a18aa150aa3a7143b76a2e4a18
yjeom <eomyj0920@gmail.com>
2022-01-19 21:45:05 +0900

    회원가입 폼

--- src/main/java/com/yjeom/pro01/memoryrestaurant/controller/MemberController.java
+++ src/main/java/com/yjeom/pro01/memoryrestaurant/controller/MemberController.java
@@ -1,11 +1,13 @@
 package com.yjeom.pro01.memoryrestaurant.controller;
 
+import com.yjeom.pro01.memoryrestaurant.domain.Member;
 import com.yjeom.pro01.memoryrestaurant.dto.MemberFormDto;
 import com.yjeom.pro01.memoryrestaurant.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,5 +19,11 @@ public class MemberController {
     public String memberForm(Model model){
         model.addAttribute("memberFormDto",new MemberFormDto());
         return "member/memberForm";
+    }
+
+    @PostMapping(value = "/member/new")
+    public String memberForm(MemberFormDto memberFormDto){
+        System.out.println("sign up");
+        return "redirect:/";
     }
 }
