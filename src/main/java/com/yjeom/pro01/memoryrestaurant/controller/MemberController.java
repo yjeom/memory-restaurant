fc4fd71998ab21d93a3f7306d5c0c71ab3290922
yjeom <eomyj0920@gmail.com>
2022-01-20 16:53:03 +0900

    -spring boot validation -회원가입

--- src/main/java/com/yjeom/pro01/memoryrestaurant/controller/MemberController.java
+++ src/main/java/com/yjeom/pro01/memoryrestaurant/controller/MemberController.java
@@ -4,26 +4,55 @@ import com.yjeom.pro01.memoryrestaurant.domain.Member;
 import com.yjeom.pro01.memoryrestaurant.dto.MemberFormDto;
 import com.yjeom.pro01.memoryrestaurant.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
+@RequestMapping(value = "/member")
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
 
-    @GetMapping(value = "/member/new")
+    @GetMapping(value = "/new")
     public String memberForm(Model model){
         model.addAttribute("memberFormDto",new MemberFormDto());
         return "member/memberForm";
     }
 
-    @PostMapping(value = "/member/new")
-    public String memberForm(MemberFormDto memberFormDto){
-        System.out.println("sign up");
+    @PostMapping(value = "/new")
+    public String memberForm(@Valid MemberFormDto memberFormDto,BindingResult bindingResult, Model model){
+        if(bindingResult.hasErrors()){
+            if(bindingResult.hasFieldErrors("name")){
+                model.addAttribute("nameError",bindingResult.getFieldError("name").getDefaultMessage());
+            }
+            if(bindingResult.hasFieldErrors("email")){
+                model.addAttribute("emailError",bindingResult.getFieldError("email").getDefaultMessage());
+            }
+            if(bindingResult.hasFieldErrors("password")){
+                model.addAttribute("passwordError",bindingResult.getFieldError("password").getDefaultMessage());
+            }
+            return "member/memberForm";
+        }
+        try{
+            String password=passwordEncoder.encode(memberFormDto.getPassword());
+            Member member=Member.builder()
+                    .name(memberFormDto.getName())
+                    .email(memberFormDto.getEmail())
+                    .password(password)
+                    .build();
+            memberService.createMember(member);
+        }catch (IllegalStateException e){
+            model.addAttribute("errorMessage",e.getMessage());
+            return "member/memberForm";
+        }
+
         return "redirect:/";
     }
 }
--- src/main/java/com/yjeom/pro01/memoryrestaurant/dto/MemberFormDto.java
+++ src/main/java/com/yjeom/pro01/memoryrestaurant/dto/MemberFormDto.java
@@ -2,14 +2,23 @@ package com.yjeom.pro01.memoryrestaurant.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 @Getter
 @Setter
 public class MemberFormDto {
 
+    @NotBlank(message = "필수 입력 값입니다.")
     private String name;
 
+    @NotBlank(message = "필수 입력 값입니다.")
+    @Email(message = "이메일 형식으로 입력해주세요.")
     private String email;
 
+    @NotBlank(message = "필수 입력 값입니다.")
+    @Length(min = 4,max = 16,message = "비밀번호는 4자이상, 16자 이하로 입력해주세요")
     private String password;
 }
